3cebd204ff32d4055232cc6fd72df532b8b93c4e
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-08 08:38:50 -0700

    Add long_description, license & attributes to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,19 @@
 name "hub"
 version "1.0.0"
 description "git + hub = github"
+long_description  IO.read(File.expand_path('../README.md', __FILE__))
 maintainer "Dr Nic Williams"
 maintainer_email "drnicwilliams@gmail.com"
+license "MIT"
+
+attribute "hub/install_path",
+  display_name: "Install path",
+  description: "Base path where bin/hub will be installed",
+  type: "string",
+  required: "optional"
+
+attribute "git/src_path",
+  display_name: "Source path",
+  description: "Path where hub git repo will be cloned",
+  type: "string",
+  required: "optional"
