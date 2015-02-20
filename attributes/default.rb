05ebc5acecc09a5f0431dc9bd1d4092631e700fc
James Cuzella <james.cuzella@lyraphase.com>
2015-02-19 22:02:54 -0700

    Fix FC019 access node attributes consistently with hash [] method
    This avoids chef/chef #2700

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default["hub"]["install_path"] = "/usr/local"
-default["hub"]["src_path"] = "#{node.hub.install_path}/src"
-default["hub"]["repo"] = "https://github.com/github/hub.git"
+default['hub']['install_path'] = '/usr/local'
+default['hub']['src_path'] = "#{node['hub']['install_path']}/src"
+default['hub']['repo'] = 'https://github.com/github/hub.git'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,6 @@
 package "git-core"
 
-directory node.hub.src_path do
+directory node['hub']['src_path'] do
   owner "root"
   group "root"
   mode "0755"
@@ -10,17 +10,17 @@ end
 
 bash "install hub" do
   user "root"
-  cwd node.hub.src_path
+  cwd node['hub']['src_path']
   code <<-BASH
   if [[ ! -d hub ]]; then
-    git clone #{node.hub.repo}
+    git clone #{node['hub']['repo']}
     cd hub
   else
     cd hub
     git pull origin master
   fi
   ./script/build
-  cp hub #{node.hub.install_path}
+  cp hub #{node['hub']['install_path']}
   BASH
   action :run
 end
